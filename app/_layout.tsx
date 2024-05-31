f383eb50782a6b5d415fd8e5afcdc542de978e3c
Victor Hofstetter <victorbarroshof@gmail.com>
2024-05-31 09:57:24 -0500

    minimize dependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { Stack } from "expo-router";
-import { BlurView } from "expo-blur";
-import { ThemeProvider, DefaultTheme } from "@react-navigation/native";
+import React from 'react';
+import { Stack } from 'expo-router';
+import { BlurView } from 'expo-blur';
+import { ThemeProvider, DefaultTheme } from '@react-navigation/native';
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from "expo-router";
+} from 'expo-router';
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: "(tabs)",
+  initialRouteName: '(tabs)',
 };
 
 const RootLayout = () => {
@@ -23,7 +23,7 @@ const RootLayoutNav: React.FC = () => {
       <BlurView />
       {/* // ! offending line */}
       <Stack>
-        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="home" />
       </Stack>
     </ThemeProvider>
   );
--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-    plugins: [
-      [
-        "module-resolver",
-        {
-          alias: {
-            app: "./app",
-            assets: "./assets",
-            components: "./components",
-            styles: "./styles",
-            utils: "./utils",
-            data: "./data",
-            hooks: "./hooks",
-          },
-        },
-      ],
-    ],
+    presets: ['babel-preset-expo'],
   };
 };
